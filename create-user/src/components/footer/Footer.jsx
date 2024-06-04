2de24d0d3ea23554322c26629bde296298c84a03
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-06-04 16:12:02 +0500

    countires app

---
+++ create-user/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className="footer-container">
+        <p>All rights reserved. <a href="https://www.youtube.com/@akhrorweb">Akhror Soliev</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
